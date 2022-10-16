e489166b6d70941f16fb007642c646c55da008a8
Sasha Guo <sashaguo37@gmail.com>
2022-10-16 00:09:06 -0700

    Add basic CRUD for items

--- Models/item.js
+++ Models/item.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const itemSchema = new mongoose.Schema({
-	todo: {
+	name: {
 		type: String,
 		required: true,
 	},
--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
-const Item = require("./Models/item");
+const Item = require("./models/item");
+const { userInfo } = require("os");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 mongoose.connect("mongodb://localhost:27017/todo");
 
@@ -12,21 +21,49 @@ db.once("open", () => {
 	console.log("Database connected");
 });
 
-app.get("/dog", (req, res) => {
-	res.send("Woof!");
+//show all items
+app.get("/items", async (req, res) => {
+	const items = await Item.find({});
+	res.render("index.ejs", { items });
+	console.log(items);
+});
+
+//create item
+app.get("/items/new", (req, res) => {
+	res.render("new.ejs");
+});
+
+//create item
+app.post("/items", async (req, res) => {
+	const newItem = new Item(req.body);
+	await newItem.save();
+	res.redirect("/items");
+});
+
+//update item
+app.get("/items/:id/edit", async (req, res) => {
+	const item = await Item.findById(req.params.id);
+	res.render("edit.ejs", { item });
+});
+
+//update item
+app.put("/items/:id", async (req, res) => {
+	const item = await Item.findByIdAndUpdate(req.params.id, req.body);
+	res.redirect(`/items/${item._id}`);
+});
+
+//show item detail
+app.get("/items/:id", async (req, res) => {
+	const item = await Item.findById(req.params.id);
+	res.render("show.ejs", { item });
+});
+
+//delete item
+app.delete("/items/:id", async (req, res) => {
+	await Item.findByIdAndDelete(req.params.id);
+	res.redirect("/items");
 });
 
 app.listen(3000, () => {
 	console.log("App is listening on port 3000!");
 });
-
-const i = new Item({
-	todo: "Test Data",
-});
-i.save()
-	.then((i) => {
-		console.log(i);
-	})
-	.catch((e) => {
-		console.log(e);
-	});
